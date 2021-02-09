0864e732b5da0857ba5fd7541c77dad638a1ed46
Andrew <andvoz@mail.ru>
2021-02-09 12:00:55 +0400

    parse object

--- GoogleSheetsHelper/Client/GoogleSheetsClient.Utils.cs
+++ GoogleSheetsHelper/Client/GoogleSheetsClient.Utils.cs
@@ -5,32 +5,64 @@ namespace AndreyPro.GoogleSheetsHelper
 {
     public partial class GoogleSheetsClient
     {
-        public static DateTime? ParseDateTime(object obj)
+        public static bool TryParseDateTime(object obj, DateTime result)
         {
             try
             {
                 if (obj == null)
-                    return DateTime.MinValue;
+                {
+                    result = default;
+                    return false;
+                }
 
                 if (obj is double d)
-                    return DateTime.FromOADate(d);
+                {
+                    result = DateTime.FromOADate(d);
+                    return true;
+                }
 
                 if (obj is long l)
-                    return DateTime.FromOADate(l);
+                {
+                    result = DateTime.FromOADate(l);
+                    return true;
+                }
 
                 if (obj is int i)
-                    return DateTime.FromOADate(i);
+                {
+                    result = DateTime.FromOADate(i);
+                    return true;
+                }
 
                 if (obj is float f)
-                    return DateTime.FromOADate(f);
+                {
+                    result = DateTime.FromOADate(f);
+                    return true;
+                }
 
                 if (double.TryParse(obj.ToString(), out var dt))
-                    return DateTime.FromOADate(dt);
+                {
+                    result = DateTime.FromOADate(dt);
+                    return true;
+                }
             }
             catch
             {
             }
-            return null;
+            return false;
+        }
+
+        public static bool TryParseDouble(object s, out double result)
+        {
+            try
+            {
+                result = Convert.ToDouble(s);
+                return true;
+            }
+            catch
+            {
+                result = default;
+                return false;
+            }
         }
     }
 }
