6171fa61ffae8a362b9dbf6cbaa6c0a88349c3bc
Andrew <andvoz@mail.ru>
2021-02-09 10:58:17 +0400

    1

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndreyPro.GoogleSheetsHelper
+{
+    public class Class1
+    {
+    }
+}
