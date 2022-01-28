bec8a423ae188c626fed582a64b2b17bbe45bdd5
VozyakovAV <andvoz@mail.ru>
2022-01-28 08:20:29 +0400

    build

--- _build.bat
+++ _build.bat
@@ -1,26 +1,27 @@
 @echo off
 set sln="GoogleSheetsHelper\GoogleSheetsHelper.csproj"
-set libMerge="Libs\ilrepack.exe"
+set libMerge="lib\ilrepack.exe"
 set pathBase="bin"
 set pathBuild="build"
 set fileDll="GoogleSheetsHelperFull.dll"
 
-rem путь до MSBuild.exe
+rem path to MSBuild.exe
 set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
 if not exist %msbuild% (
     set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
 )
 
-rem удаляем старые сборки
+echo === Delete folder %pathBase% ===
 rmdir /S /Q %pathBase%
 
-rem собираем проект в папку bin
+echo === Build project ===
 %msbuild% %sln% /t:Rebuild /p:Configuration=Debug /p:OutputPath="..\\%pathBase%\\%pathBuild%"
 
-rem мержим библиотеки в одну
+echo === Merge libs ===
 set p=%pathBase%\\%pathBuild%
 set d1=%p%\GoogleSheetsHelper.dll %p%\Newtonsoft.Json.dll
 set d2=%p%\Google.Apis.Core.dll %p%\Google.Apis.dll %p%\Google.Apis.Auth.dll %p%\Google.Apis.Auth.PlatformServices.dll %p%\Google.Apis.Sheets.v4.dll
 %libMerge% /out:"%pathBase%\\%fileDll%" %d1% %d2%
 
+echo The end
 pause
